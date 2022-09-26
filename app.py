be94d652ea06ed3d4f6743786e7682eaeaa88da0
jose3889 <jose3889@hotmail.es>
2022-09-26 17:36:35 -0300

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 import matplotlib 
-from plotly.subplots import make_subplots
+
 
 phemex= ccxt.phemex() # utilizo phemex Exchange Markets
 symbol='BTCUSD' # simbolo de la moneda
@@ -15,31 +15,11 @@ df_market=pd.DataFrame(bars, columns=['timestamp','open', 'high', 'low', 'close'
 df_market['timestamp']=pd.to_datetime(df_market['timestamp'],unit='ms')
 
 
-candlesticks = go.Candlestick(
-    x=df_market.timestamp,
-    open=df_market.open,
-    high=df_market.high,
-    low=df_market.low,
-    close=df_market.close,
-    showlegend=False
-)
 
-volume_bars = go.Bar(
-    x=df_market.timestamp,
-    y=df_market.volume,
-    showlegend=False,
-    marker={
-        "color": "rgba(128,128,128,0.5)",
-    }
-)
-
-fig = go.Figure(candlesticks)
-fig2=go.Figure(volume_bars)
-#fig = make_subplots(specs=[[{"secondary_y": True}]])
-#fig.add_trace(candlesticks, secondary_y=True)
-#fig.add_trace(volume_bars, secondary_y=False)
-fig.update_layout(title="ETH/USDC pool after Uniswap v3 deployment")
-fig.update_yaxes(title="Price $", secondary_y=True, showgrid=True)
-#fig.update_yaxes(title="Volume $", secondary_y=False, showgrid=False)
+fig = go.Figure(data=go.Ohlc(x=df_market.timestamp,
+                    open=df_market.open,
+                    high=df_market.high,
+                    low=df_market.low,
+                    close=df_market.close))
+                    
 st.plotly_chart(fig)
-st.plotly_chart(fig2)
