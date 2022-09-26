fc224373f3f723ee5a7227f87e5983712c6c3396
jose3889 <jose3889@hotmail.es>
2022-09-26 16:42:15 -0300

    first commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
+import matplotlib 
 
 phemex= ccxt.phemex() # utilizo phemex Exchange Markets
 symbol='BTCUSD' # simbolo de la moneda
