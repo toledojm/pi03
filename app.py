8fb52d4265ddb9c9314b0f2377220a9c3a84ea39
jose3889 <jose3889@hotmail.es>
2022-09-26 17:05:55 -0300

    first commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 import matplotlib 
+import plotly.express as px
 
 phemex= ccxt.phemex() # utilizo phemex Exchange Markets
 symbol='BTCUSD' # simbolo de la moneda
@@ -18,4 +19,9 @@ fig = go.Figure(data=go.Ohlc(x=df_market.timestamp,
                     high=df_market.high,
                     low=df_market.low,
                     close=df_market.close))
+
+
+fig2= px.bar(df_market.volume)
+
 st.plotly_chart(fig, use_container_width=True)
+st.bar_chart(fig2, use_container_width=True)
