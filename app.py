4739aca552073df7c122a7767a9b77a3c39fa8e7
jose3889 <jose3889@hotmail.es>
2022-09-27 20:30:51 -0300

    first

--- app.py
+++ app.py
@@ -64,16 +64,16 @@ df_market=pd.DataFrame(ohlcv, columns=['timestamp','open', 'high', 'low', 'close
 df_market['timestamp']=pd.to_datetime(df_market['timestamp'],unit='ms')
 df_market['typical'] = np.mean([df_market.high,df_market.low,df_market.close],axis=0)
 df_market['var_close']=df_market.close.pct_change()
-df_market['var_volume']=df_market.volume.pct_change()
-df_market['var_typical']=df_market.typical.pct_change()
+df_market['var_volume']=df_market.volume.pct_change()*100
+df_market['var_typical']=df_market.typical.pct_change()*100
 
 varianza=np.round(np.var(df_market.close),2)
 volume=np.round(df_market.volume.values[-1],2)
 close=np.round(df_market.close.values[-1],2)
 typical=np.round(df_market.typical.values[-1],2)
-var_close=np.round(df_market.var_close.values[-2],6)
-var_volume=np.round(df_market.var_volume.values[-2],6)
-var_typical=np.round(df_market.var_typical.values[-2],6)
+var_close=np.round(df_market.var_close.values[-2],2)
+var_volume=np.round(df_market.var_volume.values[-2],2)
+var_typical=np.round(df_market.var_typical.values[-2],2)
 
 label_price='Precio ＄'
 label_var='Varianza 📈'
@@ -93,9 +93,9 @@ def millify(n):
 
 col1, col2, col3, col4= st.columns(4)
 col1.metric(label_price, close,var_close)
-col2.metric(label_volume, millify(volume),var_volume)
+col2.metric(label_volume, millify(volume),var_volume'%')
 col3.metric(label_var, millify(varianza))
-col4.metric(label_typical, typical, var_typical)
+col4.metric(label_typical, typical, var_typical'%')
 '------------------------------------------------------------------------------------------'
 # Create subplots and mention plot grid size
 fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
