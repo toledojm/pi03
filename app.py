74fa000d7699cdece2640674a6dea2b063aac197
jose3889 <jose3889@hotmail.es>
2022-09-27 00:06:06 -0300

    first commit

--- app.py
+++ app.py
@@ -72,4 +72,7 @@ fig.add_trace(go.Bar(x=df_market.timestamp,y=df_market.volume,showlegend=False),
 
 # Do not show OHLC's rangeslider plot 
 fig.update(layout_xaxis_rangeslider_visible=True)
+
+fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
+
 st.plotly_chart(fig)
