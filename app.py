84aa7b7b3f35ac4e84845859b4c5aa60c09de54f
jose3889 <jose3889@hotmail.es>
2022-09-28 20:05:48 -0300

    first

--- app.py
+++ app.py
@@ -109,10 +109,10 @@ fig.add_trace(go.Candlestick(x=ohlcv.date,
                     open=ohlcv.open,
                     high=ohlcv.high,
                     low=ohlcv.low,
-                    close=ohlcv.close, showlegend=False), row=1, col=1)
-fig.add_trace(go.Scatter(x=ohlcv.date, y=ohlcv.media,mode='lines',marker_color='#A9A9A9',showlegend=False,line=dict(width=0.1)),row=1, col=1)
+                    close=ohlcv.close, yaxis='Precio u$s' showlegend=False), row=1, col=1)
+fig.add_trace(go.Scatter(x=ohlcv.date, y=ohlcv.media,mode='lines',marker_color='#A9A9A9',showlegend=False,line=dict(width=0.5)),row=1, col=1)
 # Bar trace for volumes on 2nd row without legend
-fig.add_trace(go.Bar(x=ohlcv.date,y=ohlcv.volume,showlegend=False,marker_color='#ff0000'), row=2, col=1)
+fig.add_trace(go.Bar(x=ohlcv.date,y=ohlcv.volume,yaxis='Precio u$s',showlegend=False,marker_color='#ff0000'), row=2, col=1)
 # Do not show OHLC's rangeslider plot 
 fig.update(layout_xaxis_rangeslider_visible=True)
 fig.update_layout(autosize=False,width=800,height=700)
