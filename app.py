da115be50fa89edcae828568ac0233b56a790153
jose3889 <jose3889@hotmail.es>
2022-09-28 10:46:55 -0300

    first commit

--- app.py
+++ app.py
@@ -113,14 +113,14 @@ col4.metric(label_typical, typical)
 
 # Create subplots and mention plot grid size
 fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
-               vertical_spacing=0.25, subplot_titles=('OHLC', 'Volume'),
+               vertical_spacing=0.25, subplot_titles=(str("Valores Históricos de "+dic_name[option]), 'Volúmen'),
                row_width=[0.4 ,0.8])
 # Plot OHLC on 1st row
 fig.add_trace(go.Candlestick(x=ohlcv['date'],
                     open=ohlcv.open,
                     high=ohlcv.high,
                     low=ohlcv.low,
-                    close=ohlcv.close,name=str("Valores Históricos de "+dic_name[option]), showlegend=False), row=1, col=1)
+                    close=ohlcv.close, showlegend=False), row=1, col=1)
 # Bar trace for volumes on 2nd row without legend
 fig.add_trace(go.Bar(x=ohlcv.date,y=ohlcv.volume,showlegend=False), row=2, col=1)
 # Do not show OHLC's rangeslider plot 
